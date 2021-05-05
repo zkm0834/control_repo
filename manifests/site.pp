9e1831a4dfb8d95ec45f148bc0345d5073bd0c3f
zkm0834 <83664713+zkm0834@users.noreply.github.com>
2021-05-05 12:25:53 -0500

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -4,7 +4,7 @@ node 'ip-172-31-1-76.us-east-2.compute.internal' {
   include role::master_server
   file { '/root/README':
     ensure => file,
-    content => $fqdn,
+    content => "Welcome to ${fqdn}\n",
   }
 }  
 node /^web/ {
