0478828f286b82672090969f82dbb5f9c66631a0
zkm0834 <83664713+zkm0834@users.noreply.github.com>
2021-05-05 14:30:34 -0500

    Create init.pp

---
+++ site/minecraft/manifests/init.pp
@@ -0,0 +1,16 @@
+class minecraft {
+  file {'/opt/minecraft',
+    ensure => directory,
+  }
+  file {'/opt/minecraft/minecraft_server.jar':
+    ensure => file,
+    source => 'https://s3.amazonaws.com/Minecraft.Download/versions/1.6.4/minecraft_server.1.6.4.jar',
+  }
+  package {'java':
+    ensure => present,
+  }  
+  file {'/etc/systemd/system/minecraft.service':
+    ensure => file,
+    source => 'puppet:///modules/minecraft.service',
+  }
+}  
