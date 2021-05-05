4d863725c903df42e95c40393195dc5ba14542a3
zkm0834 <83664713+zkm0834@users.noreply.github.com>
2021-05-05 16:12:51 -0500

    Update init.pp

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -1,12 +1,11 @@
 class minecraft {
-  file { '/opt/minecraft':
+  file {'/opt/minecraft':
     ensure => directory,
   }
   file {'/opt/minecraft/minecraft_server.jar':
     ensure => file,
     source => 'https://s3.amazonaws.com/Minecraft.Download/versions/1.6.4/minecraft_server.1.6.4.jar',
   }
-  
   package {'java':
     ensure => present,
   }  
@@ -14,8 +13,5 @@ class minecraft {
     ensure => file,
     source => 'puppet:///modules/minecraft/minecraft.service',
   }
-  service { 'minecraft':
-    ensure  => running,
-    enabled => true,
-  }
-}
+  
+}  
