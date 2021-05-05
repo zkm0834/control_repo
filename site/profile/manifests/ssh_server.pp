a813aaf21a4310f33d9cc90f0e52928b458fdda9
zkm0834 <83664713+zkm0834@users.noreply.github.com>
2021-05-05 12:53:17 -0500

    Update ssh_server.pp

--- site/profile/manifests/ssh_server.pp
+++ site/profile/manifests/ssh_server.pp
@@ -10,6 +10,6 @@ class profile::ssh_server {
 		ensure => present,
 		user   => 'root',
 		type   => 'ssh-rsa',
-		key    => 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDBKl6H7L6f3qFEg8+MuKzvr5Ec0TZURt0CobjE4SaQtGI1do2vi5cpYTFYsQV64XQ/XAsKLR7ni/Tp7IdoZN+EiSY1eFph4Bm09wU5z3521tLlchmf8dGZHl5hynC67gh5aUpkmmdI9xwEGlLyfSSjJeeouynl86f+C+B2SZ9kipzPXaonwBPmVU3KkAS3pV5eZPDE/nUS3en9Lsx0EOtNZk9TkE4WaE39LBuVi77J5r0z7dz5VXXl+4Epskib6qbD1BIb6fW9X19fZR6lP99Y2v1PdHefc45t+Uma5rX7mV4Tdokt5OOzLd4LToWDJ2bVVHk6ojNqQhxVlblw+P4R',
+		key    => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQDBKl6H7L6f3qFEg8+MuKzvr5Ec0TZURt0CobjE4SaQtGI1do2vi5cpYTFYsQV64XQ/XAsKLR7ni/Tp7IdoZN+EiSY1eFph4Bm09wU5z3521tLlchmf8dGZHl5hynC67gh5aUpkmmdI9xwEGlLyfSSjJeeouynl86f+C+B2SZ9kipzPXaonwBPmVU3KkAS3pV5eZPDE/nUS3en9Lsx0EOtNZk9TkE4WaE39LBuVi77J5r0z7dz5VXXl+4Epskib6qbD1BIb6fW9X19fZR6lP99Y2v1PdHefc45t+Uma5rX7mV4Tdokt5OOzLd4LToWDJ2bVVHk6ojNqQhxVlblw+P4R',
 	}  
 }
