d1308bd189c4279aff146d8378c43d7dc30d5540
zkm0834 <83664713+zkm0834@users.noreply.github.com>
2021-05-05 11:02:47 -0500

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -3,3 +3,11 @@ node default {
 node 'ip-172-31-1-76.us-east-2.compute.internal' {
   include role::master_server
 }  
+
+node /^web/ {
+  include role::app_server
+}
+
+node /^db/ {
+  include role::db_server
+} 
