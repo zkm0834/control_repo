988cd451b230ed6696a1ac1ed610309a89e5adc9
zkm0834 <83664713+zkm0834@users.noreply.github.com>
2021-05-06 15:13:14 -0500

    Update init.pp

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -16,12 +16,9 @@ class minecraft (
   file {'/etc/systemd/system/minecraft.service':
     ensure => file,
     source => 'puppet:///modules/minecraft/minecraft.service',
-      install_dir => $install_dir,
-    }}
   }
   service {'minecraft':
     ensure => running,
     enable => true,
   }  
-
 }  
