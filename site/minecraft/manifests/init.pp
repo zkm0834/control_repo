501e09128719175808c592f0da08a3f0903e1dfd
zkm0834 <83664713+zkm0834@users.noreply.github.com>
2021-05-06 14:27:35 -0500

    Update init.pp

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -1,7 +1,8 @@
-class minecraft {
+class minecraft (
   $url = 'https://s3.amazonaws.com/Minecraft.Download/versions/1.6.4/minecraft_server.1.6.4.jar'
   $install_dir = '/opt/minecraft'
-  file {'/opt/minecraft':
+){
+  file {$install_dir:
     ensure => directory,
   }
   file {"${install_dir}/minecraft_server.jar"}:
