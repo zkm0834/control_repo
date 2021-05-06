1d558f55510cd036970cbac45084649f23265e58
zkm0834 <83664713+zkm0834@users.noreply.github.com>
2021-05-06 14:08:20 -0500

    Update ssh_server.pp

--- site/profile/manifests/ssh_server.pp
+++ site/profile/manifests/ssh_server.pp
@@ -13,3 +13,4 @@ class profile::ssh_server {
 		key    => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQDLu3Y/4OYMPIt7r/Ef53HxAVc8vBtHAIoww4S96b4V5RngkK/Pvy24QinQKs1zcy5eBkj4NIeUV6eZuvgpa3WydJAtjLegqPTFNh0ilZT7oD6QNgoy13N8gGfnL5xbadZlFD9eALUiiZI4er6pmnVKtAU9EzhUmGMuTrz0nZFxyJkwSQ2/Xxh2ms8mo/iXHCcFe8Fjlwu5H86IZnj/MihXqMEZxyvxtVkh8jY4Jsm8/kCl2SunhmJ27EHT0LUBGq/BDNlvPHEAp6ScRp6KjJBvzPilJ6YHENiI4VxjAGjnY7mjGOCWlkNyxR1EdQCpO/UEMGX61bs+rxbk/uPXjKz5',
 	}  
 }
+
